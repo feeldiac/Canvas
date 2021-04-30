e037f3503a2660039ded68554f060adc15244137
feeldiac <feeldiac@gmail.com>
2021-04-30 08:27:27 -0500

    Initializing canvas and creating the constructor function for particles

---
+++ js/1script.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+let particleArray;
+
+// Create constructor function
+
+function Particle(x, y, directionX, directionY, size, color) {
+  this.x = x;
+  this.y = y;
+  this.directionX = directionX;
+  this.directionY = directionY;
+  this.size = size;
+  this.color = color;
+}
