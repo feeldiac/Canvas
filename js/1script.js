de7ab3de2e5b60832f938fe790c50f832230f062
feeldiac <feeldiac@gmail.com>
2021-04-30 08:40:06 -0500

    Adding a method to draw each particle and other to update a few properties. Resolving a path issue (index)

--- js/1script.js
+++ js/1script.js
@@ -14,3 +14,26 @@ function Particle(x, y, directionX, directionY, size, color) {
   this.size = size;
   this.color = color;
 }
+
+// Add draw method to particle prototype
+
+Particle.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this, this.size, 0, Math.PI * 2, false);
+  ctx.fillStyle = this.color;
+  ctx.fill();
+};
+
+//Add update method to particle prototype
+
+Particle.prototype.update = function () {
+  if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+    this.directionX = -this.directionX;
+  }
+
+  if (this.y + this.size > canvas.height || this.y - this.size < 0) {
+    this.directionY = -this.directionY;
+  }
+
+  this.draw();
+};
